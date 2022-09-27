ca2802a6b1ddf3d1e1c207c425e12bd2ca2e8b90
fauzhanFARTF <fauzan1812@gmail.com>
2022-09-28 06:12:11 +0700

    ADD DATABASE DUMP

--- app.py
+++ app.py
@@ -26,36 +26,38 @@ while lanjut:
     print("")
      
  
-    p = int(input("PILIH MENU :"))
+    p = int(input("SILAHKAN PILIH MENU : "))
     print("")
     print("")
     if(p == 1):
         mycursor.execute("SELECT * FROM user")
         myresult = mycursor.fetchall()
-        print("======================")
+        print("====== ANDA BERADA DI MENU LIHAT SELURUH USER ========")
         print("(id,nama,email,no hp)")
         for x in myresult:
             print(x)
     elif(p == 2):
-        nama = input("NAMA :")
-        email = input("EMAIL :")
-        no_hp = input("NO HP :")
+        print("========== ANDA BERADA DI MENU TAMBAH USER ============")
+        nama = input("MASUKAN NAMA : ")
+        email = input("MASUKAN EMAIL : ")
+        no_hp = input("MASUKAN NO HP : ")
         sql = "INSERT INTO user (nama, email,no_hp) VALUES (%s, %s,%s)"
         val = (nama, email,no_hp)
         mycursor.execute(sql, val)
         koneksi.commit()
         print(mycursor.rowcount, "data user berhasil di tambah")
     elif(p == 3):
-        id = input("ID USER :")
+        print("=========== ANDA BERADA DI MENU EDIT USER ==============")
+        id = input("MASUKAN ID USER :")
         mycursor.execute("SELECT * FROM user where id ="+id+" LIMIT 1")
         myresult = mycursor.fetchall()
         user = None
         for x in myresult:
             user = x
         if(user != None):
-            nama = input("NAMA ("+user[1]+") :") or user[1]
-            email = input("EMAIL ("+user[2]+") :") or user[2]
-            no_hp = input("NO HP ("+user[3]+") :") or user[3]
+            nama = input("NAMA AWAL ("+user[1]+") AKAN DIGANTI MENJADI : ") or user[1]
+            email = input("EMAIL AWAL ("+user[2]+") AKAN DIGANTI MENJADI : ") or user[2]
+            no_hp = input("NO HP AWAL ("+user[3]+") :AKAN DIGANTI MENJADI  ") or user[3]
             sql = "UPDATE user SET nama=%s,email=%s,no_hp=%s WHERE id=%s"
             val = (nama, email,no_hp,id)
             mycursor.execute(sql, val)
@@ -64,7 +66,8 @@ while lanjut:
         else:
             print("data tidak ditemukan")
     elif(p == 4):
-        id = input("ID USER :")
+        print("=========== ANDA BERADA DI MENU HAPUS USER ==============")
+        id = input("MASUKAN ID USER :")
         mycursor.execute("SELECT * FROM user where id ="+id+" LIMIT 1")
         myresult = mycursor.fetchall()
         user = None
@@ -79,6 +82,7 @@ while lanjut:
         else:
             print("data tidak ditemukan")
     elif(p == 5):
+        print("=========== ANDA BERADA DI MENU CARI USER ==============")
         id = input("MASUKAN ID USER :")
         mycursor.execute("SELECT * FROM user where id ="+id+" LIMIT 1")
         myresult = mycursor.fetchall()
